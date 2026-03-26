1a51d9c5c9c644682b62756fbeb0c638bea731e9
kumar937 <ganeshkumarvattumilli2003@gmail.com>
2026-03-26 20:19:09 +0530

    CRA and Tailwind setup

---
+++ netflix-gpt/netflix-gpt/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="align-middle">
+      <h1 className="text-3xl font-bold text-green-500 align-middle">Let's build Netflix-GPT</h1>
+      </div>
+  );
+}
+
+export default App;
